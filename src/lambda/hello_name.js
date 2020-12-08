8631fb15ec662a0381e4c001f3c1ee635c02aace
Harry <harry@umidigital.co.uk>
2020-12-08 10:42:31 +0000

    added post request

--- src/lambda/hello_name.js
+++ src/lambda/hello_name.js
@@ -1,8 +1,40 @@
-exports.handler = async (event, context) => {
-  const name = event.queryStringParameters.name || "World";
+const axios = require("axios");
 
+exports.handler = async (event, context) => {
+
+  const baseUrl = "https://api.cloudflare.com/client/v4/zones/"
+
+  const site = event.queryStringParameters.name || "NA";
+  const zoneID = "";
+
+  switch(site) {
+    case "northcote":
+      zoneID = "b1cc84bd7497f767f5c9d0de7d8ed4c0";
+      break;
+    case "umi":
+      // code block
+      break;
+    default:
+      // code block
+  }
+
+  async function makePostRequest() {
+
+    data = {
+      "purge_everything":true
+      }
+
+    let apiUrl = baseUrl + zoneID + "/purge_cache";
+    console.log(apiUrl);
+
+    let res = await axios.post(apiUrl, data);
+
+    console.log(res.data);
+}
+
+makePostRequest();
   return {
     statusCode: 200,
-    body: `Hello, ${name}`
+    body: `Success, ${site} was cleared.`
   };
 };
