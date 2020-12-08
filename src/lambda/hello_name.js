a8dc50fc975b4639d7c75591277ad6e94eb468f7
Harry <harry@umidigital.co.uk>
2020-12-08 10:52:06 +0000

    ammended axios

--- src/lambda/hello_name.js
+++ src/lambda/hello_name.js
@@ -5,6 +5,7 @@ exports.handler = async (event, context) => {
   const baseUrl = "https://api.cloudflare.com/client/v4/zones/"
 
   const site = event.queryStringParameters.name || "NA";
+
   const zoneID = "";
 
   switch(site) {
@@ -19,22 +20,34 @@ exports.handler = async (event, context) => {
   }
 
   async function makePostRequest() {
+    
+    let apiUrl = baseUrl + zoneID + "/purge_cache";
+    let postdata = JSON.stringify({"purge_everything":true});
 
-    data = {
-      "purge_everything":true
+    let config = {
+      method: 'post',
+      url: apiUrl,
+      headers: {
+        'Content-Type': 'application/json', 
+        'Authorization': 'Bearer ' + 'HHC_HSZZXogvmnzo0dm5D3-pYJzZBfUUmsmiZtlw'
+      },
+      data : postdata,
       }
 
-    let apiUrl = baseUrl + zoneID + "/purge_cache";
-    console.log(apiUrl);
-
-    let res = await axios.post(apiUrl, data);
-
-    console.log(res.data);
-}
+    let res = await axios.post(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
 
 makePostRequest();
+
   return {
     statusCode: 200,
     body: `Success, ${site} was cleared.`
   };
+
 };
