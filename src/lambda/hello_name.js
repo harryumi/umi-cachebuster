a76dd42dd299ee57f0806e2a2b6e9a4e6a561e51
Harry Fielder <harry@harryfielder.com>
2021-02-05 14:52:47 +0000

    added GHG

--- src/lambda/hello_name.js
+++ src/lambda/hello_name.js
@@ -9,7 +9,7 @@ exports.handler = async (event, context) => {
 
   let zoneID = "";
 
-  switch(site) {
+  switch (site) {
     case "northcote":
       zoneID = "b1cc84bd7497f767f5c9d0de7d8ed4c0";
       break;
@@ -22,23 +22,26 @@ exports.handler = async (event, context) => {
     case "umiversity":
       zoneID = "e9d5abb70fabd131b6e2bed6d5d86a2f";
       break;
+    case "goodhotelguide":
+      zoneID = "77e9455b6bcb92cd2c91d4fe277998e3";
+      break;
     case "hma":
       zoneID = "7063c0025728523761b2f6a1eda7d9a7";
       break;
     default:
-      // code block
+    // code block
   }
 
-  let postdata = JSON.stringify({"purge_everything":true});
+  let postdata = JSON.stringify({ "purge_everything": true });
 
-  return fetch('https://api.cloudflare.com/client/v4/zones/' + zoneID + '/purge_cache', { method: 'post',  body: postdata, headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + CLOUDFLARE_BEARER_TOKEN }})
-  .then(response => response.json())
-  .then(data => ({
-    statusCode: 200,
-    body: JSON.stringify({
-      data: data
-    })
-  }))
-  .catch(error => ({ statusCode: 422, body: String(error) }));
+  return fetch('https://api.cloudflare.com/client/v4/zones/' + zoneID + '/purge_cache', { method: 'post', body: postdata, headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + CLOUDFLARE_BEARER_TOKEN } })
+    .then(response => response.json())
+    .then(data => ({
+      statusCode: 200,
+      body: JSON.stringify({
+        data: data
+      })
+    }))
+    .catch(error => ({ statusCode: 422, body: String(error) }));
 
 };
